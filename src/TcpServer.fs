53ed85bb124dcfac8dc6bb678728882af7616ce7
Amir Dehestani <amira.dehestani@gmail.com>
2025-09-26 14:21:06 -0400

    Create unit test project

--- src/TcpServer.fs
+++ src/TcpServer.fs
@@ -5,6 +5,13 @@ open System.Net
 open System.Net.Sockets
 open Domain
 
+/// Generic TCP server that broadcasts messages to connected clients
+/// <param name="port">Port number to listen on</param>
+/// <param name="serverName">Name of the server for logging purposes</param>
+/// <param name="clientName">Name of the client for logging purposes</param>
+/// <param name="messageFormatter">Function to format messages for clients</param>
+/// <param name="stream">Stream of messages to broadcast</param>
+/// <returns>Async workflow representing the server operation</returns>
 let private createTcpServer<'T> (port: int) (serverName: string) (clientName: string) (messageFormatter: 'T -> string) (stream: IObservable<'T>) =
     async {
         let ipEndPoint = new IPEndPoint(IPAddress.Any, port)
@@ -44,6 +51,10 @@ let private createTcpServer<'T> (port: int) (serverName: string) (clientName: st
             printfn "%s stopped." serverName
     }
 
+/// Start a TCP server to broadcast access logs
+/// <param name="port">Port number to listen on</param>
+/// <param name="logStream">Stream of access log entries</param>
+/// <returns>Async workflow representing the server operation</returns>
 let startLogsTcpServer (port: int) (logStream: IObservable<AccessLog>) =
     let formatAccessLog (log: AccessLog) =
         sprintf """{"UserId": "%s", "Timestamp": "%O", "Event": "%A", "PreviousState": "%A", "NewState": "%A", "Message": "%s"}"""
@@ -56,6 +67,10 @@ let startLogsTcpServer (port: int) (logStream: IObservable<AccessLog>) =
     
     createTcpServer port "Access Logs TCP Server" "log" formatAccessLog logStream
 
+/// Start a TCP server to broadcast door state updates
+/// <param name="port">Port number to listen on</param>
+/// <param name="stateStream">Stream of door state updates</param>
+/// <returns>Async workflow representing the server operation</returns>
 let startDoorStateTcpServer (port: int) (stateStream: IObservable<DoorState>) =
     let formatDoorState (state: DoorState) =
         sprintf """{"State": "%A", "Timestamp": "%O"}"""
